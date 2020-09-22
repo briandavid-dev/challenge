a9cb962f34d0d5dde2a40cf13c1a65e91d7a8f3e
Brian Ochoa <brayad@gmail.com>
2020-09-22 14:16:27 -0300

    .

--- html/index.js
+++ html/index.js
@@ -85,5 +85,65 @@ app.listen(port, () => {
         console.log(error);
     }
 
-})()
+})();
+
+
+
+(async function restore_db2() {
+    try {
+        
+        var query = `SELECT * 
+        FROM information_schema.tables
+        WHERE table_schema = 'demo2' 
+            AND table_name = 'orders'
+        LIMIT 1`;
+
+        var promise = poolOrders.promise();
+
+        var [rows,fields] = await promise.query(query);
+
+        // en caso de no existir la tabla, la crea y le inserta data
+        if(!rows.length) {
+            var query = `
+            CREATE TABLE orders (
+                id VARCHAR(36) PRIMARY KEY,
+                user_id VARCHAR(36),
+                cart_id VARCHAR(36),
+                total_amount SERIAL,
+                create_at TIMESTAMP NULL,
+                update_at TIMESTAMP NULL,
+                delete_at TIMESTAMP NULL
+            );
+            `;
+
+            var promise = poolOrders.promise();
+
+            var [rows,fields] = await promise.query(query);
+
+            console.log("CREATE TABLE orders");
+
+            var query = `
+            INSERT INTO orders 
+            VALUES
+                ('0x98e91635fc7011eab8920242ac120008', '0x0be8add0fc6f11eab8920242ac120008', '0x5b456bc2fc6f11eab8920242ac120008', 100000, NOW(), NOW(), NOW())
+            ;
+            `;
+
+            var promise = poolOrders.promise();
+
+            var [rows,fields] = await promise.query(query);
+
+            console.log("INSERT INTO orders");
+
+
+        } else {
+            console.log("TABLE orders ok");
+        }
+        
+
+    } catch (error) {
+        console.log(error);
+    }
+
+})();
 
